52dbb88de56f39c5714253782e31c3a40f8bde20
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2020-11-01 16:01:14 -0300

    Finish responsive template base

--- src/containers/Home/home.tsx
+++ src/containers/Home/home.tsx
@@ -1,41 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from 'quarks-ui';
 
 import Breadcrumb from '../../components/Breadcrumb';
 import Header from '../Header';
 import Sidebar from '../Sidebar';
 
-const Home: React.FC = () => (
-    <Box
-        styling="column"
-        minHeight="calc(100vh - 48px)"
-        justifyContent="flex-start"
-    >
-        <Breadcrumb />
+const Home: React.FC = () => {
+    const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+    const handleSidebar = () => setIsOpenSidebar((prevState) => !prevState);
+
+    return (
         <Box
-            styling="grid"
+            styling="column"
             minHeight="calc(100vh - 48px)"
-            width="100%"
-            gridTemplateColumns="calc(100% - 393px) 393px"
+            justifyContent="flex-start"
         >
+            <Breadcrumb handleSidebar={handleSidebar} />
             <Box
-                styling="column"
-                minHeight="100%"
-                padding="24px 54px"
-                justifyContent="flex-start"
+                styling="grid"
+                minHeight="calc(100vh - 48px)"
+                width="100%"
+                gridTemplateColumns={{
+                    mobile: '100%',
+                    tablet: '100%',
+                    desktop: 'calc(100% - 393px) 393px',
+                }}
             >
-                <Header />
-            </Box>
-            <Box
-                styling="column"
-                minHeight="100%"
-                justifyContent="flex-start"
-                bg="white.default"
-            >
-                <Sidebar />
+                <Box
+                    styling="column"
+                    minHeight="100%"
+                    padding="24px 54px"
+                    justifyContent="flex-start"
+                    p={{ mobile: '12px', tablet: '24px', desktop: '24px 54px' }}
+                >
+                    <Header />
+                </Box>
+                <Sidebar isOpen={isOpenSidebar} />
             </Box>
         </Box>
-    </Box>
-);
+    );
+};
 
 export default Home;
